0d9f4f3280ff49c25760496663ca1a875be2db35
Makhothi <147424358+Makhothi@users.noreply.github.com>
2023-10-24 18:59:09 +0200

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,29 @@
 #include "main.h"
-/**
- * puts2 - prints every other character
- * @str: string
- */
 
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * puts2 - Prints one char out of 2 of a string
+ * @str: String being evaluated
+ */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
+	len = _strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(*(str + i));
 	_putchar('\n');
 }

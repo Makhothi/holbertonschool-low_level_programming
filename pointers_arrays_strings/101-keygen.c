d560ed4bf2271e83ab4804a5d1031c684740c45a
Makhothi <147424358+Makhothi@users.noreply.github.com>
2023-10-25 18:51:16 +0100

    Update 101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,32 @@
+#include <main.h>
+#include <stdio.h>
+#include <time.h>
 
+/**
+ * main -  entry point
+ * Description: generates random valid psswrd
+ * Return: Always 0 (on success).
+ */
+
+int main(void)
+{
+	int i, j, sum;
+	char psswrd[100];
+
+	sum = 0;
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		psswrd[i] = rand() % 78;
+		sum = sum + (psswrd[i] + '0');
+		putchar(psswrd[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2772 - sum - '0';
+			sum = sum + j;
+			putchar(j + '0');
+			break;
+		}
+	}
+	return (0);
+}

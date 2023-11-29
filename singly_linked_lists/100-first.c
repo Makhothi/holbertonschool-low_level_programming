e33a3d82518cc3b24f4cbeca9ac805cc594befdc
Makhothi <147424358+Makhothi@users.noreply.github.com>
2023-11-29 18:08:37 +0200

    Update 100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * bmain - function executed before main
+ * Return: no return.
  */
-int main(void)
+
+void __attribute__ ((constructor)) bmain()
 {
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
     return (0);

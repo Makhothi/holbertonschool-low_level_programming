4a76a8a111f2a90d5c3f36c4ca5540d3a6029968
Makhothi <147424358+Makhothi@users.noreply.github.com>
2023-11-29 18:33:40 +0200

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
-* print_dlistint - prints elements of a dlistint_t list.
-* @h: pointer to the head of the list.
-*
-*Return: number of nodes.
-*/
+ * print_dlistint - Prints integers from doubly linked list
+ * @h: Pointer to head of list
+ *
+ * Return: Number of nodes printed
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t count = 0;
+	size_t numelem = 0;
+	const dlistint_t *temp;
 
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-count++;
-}
-return (count);
+	temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		numelem++;
+	}
+
+	return (numelem);
 }

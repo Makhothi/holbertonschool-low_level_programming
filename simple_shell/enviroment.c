11f1e9b8603e32b81908930e4479e360601322e7
Makhothi <147424358+Makhothi@users.noreply.github.com>
2023-12-19 17:04:04 +0000

    Create enviroment.c

---
+++ simple_shell/enviroment.c
@@ -0,0 +1,16 @@
+#include "shells.h"
+
+/**
+ * print_env - prints the environment
+ * Return: zero
+ */
+
+int print_env()
+{
+int i = 0;
+while (env[i])
+{
+printf("%s\n", env[i++]);
+}
+return (0);
+}
